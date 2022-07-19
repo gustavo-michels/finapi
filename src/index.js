dc53d0df4e0e564c1246cf8a265e77c4dd096eb7
Gustavo H. Michels <ghmichels@gmail.com>
2022-07-19 13:47:10 -0300

    added account removal feature

--- src/index.js
+++ src/index.js
@@ -130,4 +130,12 @@ app.get("/account", verifyIfExistsAccountCPF, (req, res) => {
   return res.json(customer)
 })
 
+app.delete("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req
+
+  customers.splice(customer, 1)
+
+  return res.status(204).send()
+})
+
 app.listen(3333)
